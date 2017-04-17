1468b7ac37b4f1649d43c5530fee1e2628a9e45f
Brandon Shields <braven112@gmail.com>
2017-04-17 08:35:40 -0700

    released phase one

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,3 +8,11 @@ $('#league_chat caption span a').html('<svg class="icon-comments"><use xlink:hre
 //Move login button info
 $('.welcome').children().detach().appendTo('#loginlink .svg-text');
 $('#loginlink .svg-text a').eq(1).addClass('commish-link');
+
+setTimeout( function(){ 
+    // Do something after 1 second 
+    $('#league_chat b').parent().addClass('personal-im');
+  }  , 1000 );
+$(document).ready(function() {
+$('#power_rank caption span').text('All-Play Record');
+});
