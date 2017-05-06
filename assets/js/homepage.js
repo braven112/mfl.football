2aa4fb5f8009ce7ac347a12b928e1863be27f929
Brandon Shields <braven112@gmail.com>
2017-05-05 19:45:40 -0700

    adding native support of league chat personal messages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,23 +8,3 @@ $('#league_chat caption span a').html('<svg class="icon-comments"><use xlink:hre
 //Move login button info
 $('.welcome').children().detach().appendTo('#loginlink .svg-text');
 $('#loginlink .svg-text a').eq(1).addClass('commish-link');
-$(window).load(function() {
-setTimeout( function(){ 
-    // Do something after .1 second 
-    $('#league_chat b').parent().addClass('personal-im');
-  }  , 100 );
-setTimeout( function(){ 
-    // Do something after .2 second 
-    $('#league_chat b').parent().removeClass().addClass('personal-im');
-  }  , 200 );
-setTimeout( function(){ 
-    // Do something after .5 second 
-    $('#league_chat b').parent().removeClass().addClass('personal-im');
-  }  , 500 );
-
-setTimeout( function(){ 
-    // Do something after .5 second 
-    $('#league_chat b').parent().removeClass().addClass('personal-im');
-  }  , 1500 );
-
-});
