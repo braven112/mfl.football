236d55d82fb03e811b6fd8563ed00da308a2f78f
Brandon Shields <braven112@gmail.com>
2017-06-14 00:15:16 -0700

    updates to index

--- assets/js/global-qa.js
+++ assets/js/global-qa.js
@@ -21,6 +21,7 @@ jQuery(document).ready(function() {
     //customer wrapper
     $(".report").wrap("<div class='report-wrapper'></div>");
     $("#outerMFLScoreboardDiv .report-wrapper .report,#lineup-form .report-wrapper .report,#body_board_show .report-wrapper .report-wrapper .report").unwrap();
+    $(".report-wrapper .report-wrapper .report").unwrap(); 
 
     // Add Login to menu
     $('.myfantasyleague_menu > ul').append('<li class="has-sub sub-default" id="slide-menu-login"><a>Login</a><b aria-haspopup="true" aria-controls="p50"></b><input id="sub50" type="checkbox"><label for="sub50"><span></span></label><ul id="p50"></ul></li>');
