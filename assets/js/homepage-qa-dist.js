1bf125893e6c6fcd6c5c137283f55d2d1c8b1ccf
Brandon Shields <braven112@gmail.com>
2018-03-18 22:55:26 -0700

    fixing icon issue with owner activity module & updating js for auctions

---
+++ assets/js/homepage-qa-dist.js
@@ -0,0 +1,4 @@
+jQuery("#curr_auctions").prepend($("#curr_auctions .reportfooter:last")), jQuery("#curr_auctions .reportfooter td[colspan=3]").attr("colspan", 4), 
+jQuery("#curr_auctions .reportfooter:first").removeClass().addClass("btn-primary"), 
+jQuery("#league_chat caption span a").html('<svg class="icon-comments"><use xlink:href="#icon-comments"></use></svg>'), 
+jQuery(".welcome").children().detach().appendTo("#loginlink .svg-text"), jQuery("#loginlink .svg-text a").eq(1).addClass("commish-link");
--- assets/js/homepage-qa.js
+++ assets/js/homepage-qa.js
@@ -1,4 +1,3 @@
-jQuery('#curr_auctions').prepend($('#curr_auctions .reportfooter:last'));
 jQuery("#curr_auctions .reportfooter td[colspan=3]").attr('colspan',4);
 jQuery("#curr_auctions .reportfooter:first").removeClass().addClass('btn-primary');
 
